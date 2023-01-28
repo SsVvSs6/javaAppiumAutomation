4d7cf7a0740802d10d6af6f5ead0f93d221a0720
Svetlana Seredenko <svetlana97sered@gmail.com>
2023-01-28 14:33:54 +0300

    Test 3 is added

--- src/lesson1/test1/MainClass.java
+++ src/lesson1/test1/MainClass.java
@@ -3,6 +3,7 @@ package lesson1.test1;
 public class MainClass {
 
     private static int class_number = 20;
+    private static String class_string = "Hello, world";
 
     public static int getLocalNumber() {
         return 14;
@@ -10,5 +11,9 @@ public class MainClass {
 
     public static int getClassNumber() {
         return class_number;
+    }
+
+    public static String getClassString() {
+        return class_string;
     }
 }
--- src/lesson1/test1/MainClassTest.java
+++ src/lesson1/test1/MainClassTest.java
@@ -16,4 +16,16 @@ public class MainClassTest {
         int highNumber = 45;
         Assert.assertTrue("Number is less than 45", highNumber < MainClass.getClassNumber());
     }
+
+    @Test
+    public void testGetClassString() {
+        String firstText = "hello";
+        String secondText = "Hello";
+        Assert.assertTrue("Text doesn't contain word 'Hello' or 'hello'",
+                isTextContainsAnyOfTwoWords(MainClass.getClassString(), firstText, secondText));
+    }
+
+    private boolean isTextContainsAnyOfTwoWords(String text, String firstWord, String secondWord) {
+        return text.contains(firstWord) | text.contains(secondWord);
+    }
 }
